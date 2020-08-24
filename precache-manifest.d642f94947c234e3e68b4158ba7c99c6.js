cb2f00c286c56c228c483fe1d1cafa390cd17ca7
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-08-24 15:06:40 -0700

    first commit

---
+++ precache-manifest.d642f94947c234e3e68b4158ba7c99c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dc12c18e4dfa262705a",
+    "url": "/trialforkit/static/css/main.ac210ef9.chunk.css"
+  },
+  {
+    "revision": "7dc12c18e4dfa262705a",
+    "url": "/trialforkit/static/js/main.7dc12c18.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/trialforkit/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "4a2be9ead905e613a81f",
+    "url": "/trialforkit/static/js/runtime~main.4a2be9ea.js"
+  },
+  {
+    "revision": "e758936d4374eee2ed653d3f42f5f0a4",
+    "url": "/trialforkit/index.html"
+  }
+];
